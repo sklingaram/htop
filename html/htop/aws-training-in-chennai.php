a5a216aad5de7d4c1d62441474f2d95f1026f733
Selva <developer.selvak@gmail.com>
2018-01-27 18:38:37 +0530

    Add front page changes

---
+++ html/htop/aws-training-in-chennai.php
@@ -0,0 +1,122 @@
+<!Doctype html>
+<html>
+<head>
+<title>AWS Training in Chennai</title>
+<meta charset="UTF-8">
+<link rel="canonical" href="http://zerobugacademy.com/aws-training-in-chennai.php">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="description" content="Zerobugacademy provides (AWS) Amazon Web Services Training In Chennai,Our Amazon Web Services Training is fully a Practical Oriented.Call for demo.">
+<meta property="og:url" content="http://zerobugacademy.com/aws-training-in-chennai.php">
+<meta property="og:site_name" content="Zerobug Academy | No.1 Training Institute in Chennai">
+<meta property="og:image" content="http://zerobugacademy.com/images/zerobugacademy.png">
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:description" content="Zerobugacademy provides (AWS) Amazon Web Services Training In Chennai,Our Amazon Web Services Training is fully a Practical Oriented.Call for demo.">
+<meta name="twitter:title" content="AWS Training in Chennai">
+<meta name="twitter:site" content="@zerobugacademy">
+<meta name="twitter:image" content="http://zerobugacademy.com/images/zerobugacademy.png">
+<meta name="twitter:creator" content="@zerobugacademy">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<?php include "header.php"; ?>
+<?php include "navbar.php"; ?>
+<!--<div class="page-header" style="margin:0px;background-color:#fd9b9b">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="contes_header">
+					<div class="contes_title">
+						<h2>Oracle Training</h2>
+					</div>
+				</div>
+			<ul class="breadcrumb">
+			<li class="bread_li"><a href="index.php">Home</a></li>
+			<li class="active">course</li>
+			</ul>
+			</div>
+			</div>
+	</div>
+</div>--> 
+<section class="shortbanner">
+<img src="images/zerobug training institute in chennai.png" alt="aws training institute in chennai">
+</section>
+<section class="innergraypart">
+    <div class="container"> <span class="left">
+      <h1>AWS Training in Chennai</h1>
+      <h2>AWS (Amazon Web Services) Training Institute in chennai With Live Projects</h2>
+     
+      <div class="clear"></div>
+    </div>
+</section>
+  
+<div class="course_list">
+	<div class="container">
+		<div class="row" style="padding-top:20px;">
+			<div class="col-lg-8">
+			<div class="duration">
+            <img src="images/demo.jpg" alt="http://www.zerobugacademy.com/">
+            </div>
+		<div class="course_details">
+		<p>Amazon Web Service is the widely used cloud computing platform among various public clouds since it offers cost effective and secured cloud computing platform. Amazon Web Service is used to develop highly sophisticated application with high scalability and flexibility.AWS offers competitive attracting which attracts companies to host and manage their applications at reasonable cost moreover it provides lots of free tools used automation.   </p>
+		<h2>Best Institute for Learning AWS</h2>
+		<p>Zero bug Academy Provides best AWS Training in Chennai at reasonable cost and best placement support. The AWS training sessions are handled by top notch IT experts in Chennai who are capable of teaching concepts with real time examples. The AWS Training in Chennai Syllabus are designed according current requirement of IT companies moreover zero bug academy provides more practical class which help you clear the interviews and certification easily. After completion of course zero bug academy will arrange you  interviews in leading software companies in Chennai so it right time to join AWS training at Zero Academy.
+</p>
+
+		</div>
+		</div>
+		<div class="col-lg-4">
+				<div class="training_course">
+					<h2>Training Courses</h2>
+					<div class="sidebar_course">
+						<ul>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">AWS And J2EE Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Dot Net Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Software Testing Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Cloud Computing Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Big Data Hadoop Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Android Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">IOS Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Web Designing Course In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">PHP Training In Chennai
+						</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Digital Marketing Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">SEO Training In Chennai
+						</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Oracle Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Selenium Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">QTP Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">LoadRunner Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Mobile Testing Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">C / C++ Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">UNIX Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Placement Training In Chennai</a></li>
+						<li class="last_li"><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">German Classes In Chennai</a></li>
+						</ul>
+					</div>
+					</div>
+				</div>
+			</div>
+			</div>
+	</div>
+</div>
+
+<?php include "footer.php"; ?>	
---
+++ html/htop/web-design-training-in-chennai.php
@@ -0,0 +1,101 @@
+<?php include "header.php"; ?>
+<?php include "navbar.php"; ?>
+<!--<div class="page-header" style="margin:0px;background-color:#fd9b9b">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="contes_header">
+					<div class="contes_title">
+						<h2>Oracle Training</h2>
+					</div>
+				</div>
+			<ul class="breadcrumb">
+			<li class="bread_li"><a href="index.php">Home</a></li>
+			<li class="active">course</li>
+			</ul>
+			</div>
+			</div>
+	</div>
+</div>--> 
+<section class="shortbanner">
+<img src="images/zerobug training institute in chennai.png" alt="web design training institute in chennai">
+</section>
+<section class="innergraypart">
+    <div class="container"> <span class="left">
+      <h1>Web Design Training in Chennai</h1>
+      <h2>Web Design Training Institute in chennai With Live Projects</h2>
+      <div class="clear"></div>
+    </div>
+</section>
+  
+<div class="course_list">
+	<div class="container">
+		<div class="row" style="padding-top:20px;">
+			<div class="col-lg-8">
+			<div class="duration">
+            	<img src="images/demo.jpg" alt="http://www.zerobugacademy.com/">
+            </div>
+		<div class="course_details">
+		<p>Web Design is a programming language used mainly to develop web applications and platforms. Being platform-independent, it is popularly used everywhere as it can be accessed and run easily from any platform. Applications developed from Web Design are used for many devices such as computers, smartphones, laptops, car navigation systems, media players, etc. Web Design’s API is a major reason for its success and also the development tools such as Eclipse and Netbeans. Web Design has many open source libraries that have made the development process very swift with reduced cost.One major factor that is very attractive about Web Design is its availability. </p>
+		<p>It is free of cost and that makes it easily approachable for all the first-timers and beginners. The documentation support via Web DesignDoc is of great use to all those who are not aware of the technicalities that come with programming.</p>
+				</div>
+				
+				
+			</div>
+		<div class="col-lg-4">
+				<div class="training_course">
+					<h2>Training Courses</h2>
+					<div class="sidebar_course">
+						<ul>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Web Design And J2EE Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Dot Net Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Software Testing Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Cloud Computing Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Big Data Hadoop Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Android Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">IOS Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Web Designing Course In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">PHP Training In Chennai
+						</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Digital Marketing Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">SEO Training In Chennai
+						</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Oracle Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Web Design Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">QTP Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">LoadRunner Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Mobile Testing Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">C / C++ Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">UNIX Training In Chennai</a></li>
+						<li><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">Placement Training In Chennai</a></li>
+						<li class="last_li"><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<a href="">German Classes In Chennai</a></li>
+						</ul>
+					</div>
+					</div>
+				</div>
+			</div>
+			</div>
+	</div>
+</div>
+
+<?php include "footer.php"; ?>	
